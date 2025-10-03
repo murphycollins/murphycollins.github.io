f3d474e67692a288eaeacbb5e0a8573dda6b2b50
murphycollins <murphycollins@my.uopeople.edu>
2025-10-03 17:24:57 +0100

    Delete calculator directory

--- calculator/script.js
+++
@@ -1,6 +0,0 @@
-function press(val){document.getElementById("calc-screen").value+=val;}
-function calculate(){
-  const screen=document.getElementById("calc-screen");
-  try{screen.value=eval(screen.value);}catch{screen.value="Error";}
-}
-function clearScreen(){document.getElementById("calc-screen").value="";}
